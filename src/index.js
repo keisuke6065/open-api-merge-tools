04104fd5f578d7711b892ac43c6efa6a2b1ba65e
Yuki Morota <cororo6@gmail.com>
2018-10-26 13:28:55 +0900

    create output!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 yaml = require('js-yaml');
 fs   = require('fs');
 
+
 function readYamlFile(filePath) {
     try {
         const targetYamlFile = fs.readFileSync(filePath);
@@ -10,8 +11,23 @@ function readYamlFile(filePath) {
     }
 }
 
+function writeYamlFile(data) {
+    try {
+        const dirname = './output';
+        if (!fs.existsSync(dirname)) fs.mkdirSync(dirname);
+        fs.writeFileSync("./output/openapi.yaml", yaml.safeDump(data));
+        return "Successful XD!!! \n ./output/openapi.yaml \n";
+    } catch (e) {
+        return "Failed :("
+    }
+}
+
 const test = readYamlFile(process.argv[2]);
 console.log(test);
 
 const doc = readYamlFile(process.argv[3]);
 console.log(doc);
+
+const output =  writeYamlFile(test);
+console.log(output);
+
