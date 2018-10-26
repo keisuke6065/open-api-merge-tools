bc7e728b36385a3bce571a29eaed34b60bc6eeb9
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2018-10-26 13:14:13 +0900

    change function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 yaml = require('js-yaml');
 fs = require('fs');
 
-function readYamlFile(filePath) {
+const readYamlFile= (filePath) => {
     try {
         const targetYamlFile = fs.readFileSync(filePath);
         return yaml.safeLoad(targetYamlFile, 'utf8');
     } catch (e) {
         console.log(e)
     }
-}
+};
 
 const env = process.env;
 console.log(env);
@@ -19,7 +19,7 @@ console.log(test);
 const doc = readYamlFile(process.argv[3]);
 console.log(doc);
 
-const targetENV = process.argv[4];
-console.log(targetENV.replace('-', ''));
+const targetENV = process.argv[4].replace('-', '');
+console.log(targetENV);
 
 
