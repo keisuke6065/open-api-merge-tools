3094b63c64ca20ab9c7fccc4c871615e38b16fb2
Yuki Morota <cororo6@gmail.com>
2018-10-26 15:27:49 +0900

    apply formatter

--- src/index.js
+++ src/index.js
@@ -11,31 +11,31 @@ const readYamlFile = (filePath) => {
   }
 };
 const readYamlEnv = (envFile, targetENV) => {
-    if (envFile.propertyIsEnumerable(targetENV)) {
-        return envFile[targetENV];
-    }
-    throw new Error('env yaml not target')
+  if (envFile.propertyIsEnumerable(targetENV)) {
+    return envFile[targetENV];
+  }
+  throw new Error('env yaml not target');
 };
 
 const getOptions = (args) => {
-    if  (args.length <  5){
-      throw new Error('invalid arguments length.')
-    }
-    const options = {};
-    options.apiFilePath = args[2];
-    options.envFilePath = args[3];
-    options.targetENV = args[4].replace('-', '');
-    return options;
+  if (args.length < 5) {
+    throw new Error('invalid arguments length.');
+  }
+  const options = {};
+  options.apiFilePath = args[2];
+  options.envFilePath = args[3];
+  options.targetENV = args[4].replace('-', '');
+  return options;
 };
-const writeYamlFile= (data, filename) => {
-    try {
-        const dirname = './output';
-        if (!fs.existsSync(dirname)) fs.mkdirSync(dirname);
-        fs.writeFileSync('./output/' + filename, yaml.safeDump(data));
-        return 'Successful XD!!! \n ./output/' + filename + '\n';
-    } catch (e) {
-        return 'Failed :(';
-    }
+const writeYamlFile = (data, filename) => {
+  try {
+    const dirname = './output';
+    if (!fs.existsSync(dirname)) fs.mkdirSync(dirname);
+    fs.writeFileSync('./output/' + filename, yaml.safeDump(data));
+    return 'Successful XD!!! \n ./output/' + filename + '\n';
+  } catch (e) {
+    return 'Failed :(';
+  }
 };
 
 const envLocal = process.env;
@@ -44,20 +44,22 @@ const options = getOptions(process.argv);
 const openApiFile = readYamlFile(options.apiFilePath);
 // console.log(openApiFile);
 
-const envFile = readYamlEnv(readYamlFile(options.envFilePath), options.targetENV);
+const envFile = readYamlEnv(
+    readYamlFile(options.envFilePath),
+    options.targetENV
+);
 console.log(envFile);
 
-
 const replaceEnv = (x) => {
-    if (x.length !== undefined) {
-        const envs = Object.keys(envFile);
-        for (let i = 0; i < envs.length; i++) {
-            if (_.isMatch(x, `{env.${envs[i]}`)) {
-                return envFile[envs[i]];
-            }
-        }
-        return x
+  if (x.length !== undefined) {
+    const envs = Object.keys(envFile);
+    for (let i = 0; i < envs.length; i++) {
+      if (_.isMatch(x, `{env.${envs[i]}`)) {
+        return envFile[envs[i]];
+      }
     }
-    return _.mapValues(x, (a) => replaceEnv(a));
+    return x;
+  }
+  return _.mapValues(x, (a) => replaceEnv(a));
 };
 console.log(_.mapValues(openApiFile, (x) => replaceEnv(x)));
